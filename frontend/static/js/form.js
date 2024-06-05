7a480fa4a9a5534d0f2084327b59ba60127d3b94
jiisanda <jiisanda@protonmail.com>
2024-06-05 17:27:31 +0530

    update frontend

--- frontend/static/js/form.js
+++ frontend/static/js/form.js
@@ -24,7 +24,14 @@ document.getElementById('upload-form').addEventListener('submit', function(event
     .then(data => {
         let parsedData = JSON.parse(data);
         let responseDiv = document.getElementById('response');
-        responseDiv.textContent = JSON.stringify(parsedData, null, 2);
+        // Clear any existing content in the response div
+        responseDiv.innerHTML = '';
+        // Create a new 'pre' element
+        let pre = document.createElement('pre');
+        // Set the text content of the 'pre' element to the pretty printed JSON
+        pre.textContent = JSON.stringify(parsedData, null, 2);
+        // Append the 'pre' element to the 'response' div
+        responseDiv.appendChild(pre);
     })
     .catch(error => console.error('Error:', error));
 });
